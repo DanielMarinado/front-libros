26ec36b2da78c9cf60a49449c696d1b97bdc60ee
Daniel Marinado <daniel.marinado@gmail.com>
2022-04-23 23:04:09 -0400

    elimina default usr

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -16,9 +16,9 @@ const Login = ({ history }) => {
   useCurrentItemHeader();
   const dispatch = useDispatch();
   const { user } = useSelector((state) => ({ ...state }));
-  const [email, setEmail] = useState("mchamorro@escalab.academy");
+  const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
-  const [password, setPassword] = useState("chillan2022");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     !history.location.state && user?.token && history.push("/");
